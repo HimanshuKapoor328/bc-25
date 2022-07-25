ab492500c0f9980de7002f18a0729d3322ac73bd
himank328 <himanshu.kapoor@zemosolabs.com>
2022-07-25 12:35:06 +0530

    first commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: ['./src/index.tsx'],
+    vendor: ['react', 'react-dom'],
+  },
+  output: {
+    path: __dirname + '/dist/',
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+  },
+  devServer: {
+    static: './dist',
+    open: true,
+    historyApiFallback: true,
+    allowedHosts: [
+      'find.job.example.com',
+      'jobfind.com'
+   ]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: '/node_modules',
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
