5bcb4cb12b29a01c1eb526d7ed908ff2999d6d5e
himank328 <himanshu.kapoor@zemosolabs.com>
2022-08-08 17:57:33 +0530

    cicd for sonar 1.0.8

--- p1.py
+++ p1.py
@@ -3,7 +3,7 @@
 import requests,json,sys,os
 
 ACCESS_TOKEN=os.environ["ACCESS_TOKEN"]
-REATING=os.environ["VAULE1"]
+REATING=os.environ["VALUE1"]
 send_by="Himanshu"
 sub= "bug report"
 msg= "Current Quality Status is " + REATING + ", https://sonarcloud.io/dashboard?id=HimanshuKapoor328_bc-25&branch=master" 
